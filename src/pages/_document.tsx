04374acc2b8d6d3acb1c185d1950c731160500f5
Teodor Nicolau <teodornicolau@Teodors-MacBook-Pro.local>
2026-01-24 10:52:51 +0200

    chore: add google site verification meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ro">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="0tGUhI-DkMYjzseSFYHN-gVOVxwKD8L6zB9ZkjIiZgQ" />
+      </Head>
       <body>
         <Main />
         <NextScript />
